97cff34c733eded903c2afe9623696788101e9b6
siyuduan6 <74063021+siyuduan6@users.noreply.github.com>
2021-05-14 01:00:09 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -100,10 +100,7 @@ def year_pick():
 def vio_year():
     rl_vio1 = doc(1).dropna(subset=["MONTH"])
     rl_vio2 = doc(2).dropna(subset=["MONTH"])
-    vio3 =pd.DataFrame(rl_vio1).merge(pd.DataFrame(rl_vio2), on = "DATE").rename(
-    columns={"VIOLATIONS_x": "Red Light", "VIOLATIONS_y": "Speed"})
-    st.area_chart(vio3.set_index("DATE")[["Red Light", "Speed"]], width =650)   
-    year = st.select_slider("Year", options=[2015, 2016, 2017, 2018, 2019, 2020, 2021], value=2018)
+
     vio1 = rl_vio1[rl_vio1["YEAR"] == year].groupby("MONTH")["VIOLATIONS"].sum()
     vio2 = rl_vio2[rl_vio2["YEAR"] == year].groupby("MONTH")["VIOLATIONS"].sum()
     vio = pd.DataFrame(vio1).merge(pd.DataFrame(vio2), left_index=True, right_index=True).rename(
@@ -111,6 +108,10 @@ def vio_year():
     rl_vio1["DATE"] = pd.to_datetime(rl_vio1["YEAR"].astype("str").str.cat(rl_vio1["MONTH"].astype("str"), sep = ' '))
     rl_vio2["DATE"] = pd.to_datetime(rl_vio2["YEAR"].astype("str").str.cat(rl_vio2["MONTH"].astype("str"), sep = ' '))
 
+    vio3 =pd.DataFrame(rl_vio1).merge(pd.DataFrame(rl_vio2), on = "DATE").rename(
+    columns={"VIOLATIONS_x": "Red Light", "VIOLATIONS_y": "Speed"})
+    st.area_chart(vio3.set_index("DATE")[["Red Light", "Speed"]], width =650)   
+    year = st.select_slider("Year", options=[2015, 2016, 2017, 2018, 2019, 2020, 2021], value=2018)
     fig = plt.figure(figsize=(7, 4))  # Create matplotlib figure
     ax = fig.add_subplot(111)  # Create matplotlib axes
     ax2 = ax.twinx()  # Create another axes that shares the same x-axis as ax.
