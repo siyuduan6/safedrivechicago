f50300a1a13a185b4852d59e91cc1389c9b03e69
siyuduan6 <74063021+siyuduan6@users.noreply.github.com>
2021-05-14 00:36:33 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -100,6 +100,8 @@ def year_pick():
 def vio_year():
     rl_vio1 = doc(1).dropna(subset=["MONTH"])
     rl_vio2 = doc(2).dropna(subset=["MONTH"])
+    rl_vio1["DATE"]] = pd.to_datetime(rl_vio1["YEAR"].str.cat(rl_vio1["MONTH"], sep = ' '))
+    rl_vio2["DATE"]] = pd.to_datetime(rl_vio2["YEAR"].str.cat(rl_vio2["MONTH"], sep = ' '))
     year = st.select_slider("Year", options=[2015, 2016, 2017, 2018, 2019, 2020, 2021], value=2018)
     vio1 = rl_vio1[rl_vio1["YEAR"] == year].groupby("MONTH")["VIOLATIONS"].sum()
     vio2 = rl_vio2[rl_vio2["YEAR"] == year].groupby("MONTH")["VIOLATIONS"].sum()
@@ -115,6 +117,8 @@ def vio_year():
     ax.set_xlabel("Month")
     plt.grid(False)
     plt.xticks(rotation=45)
+    st.area_chart(rl_vio1[["DATE","VIOLATIONS"]])
+    st.area_chart(rl_vio2[["DATE","VIOLATIONS"]])            
     return fig
 
 def stack_bar_chart():
